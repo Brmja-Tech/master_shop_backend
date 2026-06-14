373f1e7c15d9efeb9fc38efc4f31597322a713ca
Karen Kamal <karenkamal24@gmail.com>
2026-06-14 17:26:20 +0300

    seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,13 +12,57 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'image'                => 'uploads/images/image.png',
-            'name'                 => 'User',
-            'email'                => 'user@gmail.com',
-            'phone'                => '1234567890',
-            'email_verified_at'    => now(),
-            'password'             => bcrypt('password'),
+        User::insert([
+            [
+                'image'             => 'uploads/images/image.png',
+                'name'              => 'Ahmed Mohamed',
+                'email'             => 'ahmed@gmail.com',
+                'phone'             => '01012345678',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ],
+            [
+                'image'             => 'uploads/images/image.png',
+                'name'              => 'Mohamed Ali',
+                'email'             => 'mohamed@gmail.com',
+                'phone'             => '01112345678',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ],
+            [
+                'image'             => 'uploads/images/image.png',
+                'name'              => 'Sara Ahmed',
+                'email'             => 'sara@gmail.com',
+                'phone'             => '01212345678',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ],
+            [
+                'image'             => 'uploads/images/image.png',
+                'name'              => 'Omar Hassan',
+                'email'             => 'omar@gmail.com',
+                'phone'             => '01512345678',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ],
+            [
+                'image'             => 'uploads/images/image.png',
+                'name'              => 'Mona Khaled',
+                'email'             => 'mona@gmail.com',
+                'phone'             => '01098765432',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'created_at'        => now(),
+                'updated_at'        => now(),
+            ],
         ]);
     }
 }
