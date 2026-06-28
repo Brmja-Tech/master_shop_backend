cd256af79f0d2e872adb1c840ed486f14d390b1a
Karen Kamal <karenkamal24@gmail.com>
2026-06-28 10:10:36 +0300

    feat: add Paymob configuration file with environment-based credentials and endpoints

--- config/paymob.php
+++ config/paymob.php
@@ -5,8 +5,8 @@ return [
     'integration_id' => env('PAYMOB_INTEGRATION_ID'),
     'iframe_id' => env('PAYMOB_IFRAME_ID'),
     'hmac_secret' => env('PAYMOB_HMAC_SECRET'),
-    'callback_url' => env('PAYMOB_CALLBACK_URL', 'https://mastershop.betamoneta.com/api/paymob/callback'),
-    'response_url' => env('PAYMOB_RESPONSE_URL', 'https://mastershop.betamoneta.com/api/paymob/response'),
+    'callback_url' => env('PAYMOB_CALLBACK_URL', 'https://mastershop.brmja.cloud/api/paymob/callback'),
+    'response_url' => env('PAYMOB_RESPONSE_URL', 'https://mastershop.brmja.cloud/api/paymob/response'),
     'refund_endpoint' => env('PAYMOB_REFUND_ENDPOINT', 'https://accept.paymob.com/api/acceptance/void_refund/refund'),
     'void_endpoint' => env('PAYMOB_VOID_ENDPOINT', 'https://accept.paymob.com/api/acceptance/void_refund/void'),
 ];
